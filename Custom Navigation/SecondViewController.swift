9342ed159c50914ac6e43047b1f33e7470efd36b
Bob Wakefield <bob@cockleburr.net>
2015-12-28 15:19:38 -0600

    How did the SecondViewController class get left out?

---
+++ Custom Navigation/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  Custom Navigation
+//
+//  Created by Bob Wakefield on 12/28/15.
+//  Copyright © 2015 Bob Wakefield. All rights reserved.
+//
+
+import UIKit
+
+
+class SecondViewController: UIViewController {
+
+}
